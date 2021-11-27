e9f1a852bc25b442892a8d3e388c0259ba1e135a
geoandr-01 <geoandr@live.unc.edu>
2021-11-26 23:01:14 -0500

    oops

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -64,7 +64,7 @@ export const tenTimes = multiplyBy(10);
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-    return tenTimes(10)
+    return tenTimes(50)
 };
 
 
